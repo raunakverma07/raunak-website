97ba1ea93c40e95d57901bde2f465211b8b9ed28
Raunak Verma <vermaraunak766@gmail.com>
2026-04-10 16:51:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ function scrollToSection(){
     behavior: "smooth"
   });
 }
+function toggleTheme(){
+
+  document.body.classList.toggle("light");
+
+  const btn = document.querySelector(".theme-btn");
+
+  if(document.body.classList.contains("light")){
+    btn.innerHTML = "☀️";
+    localStorage.setItem("theme", "light");
+  } else {
+    btn.innerHTML = "🌙";
+    localStorage.setItem("theme", "dark");
+  }
+}
+
+/* Load saved theme */
+window.onload = function(){
+  const saved = localStorage.getItem("theme");
+
+  if(saved === "light"){
+    document.body.classList.add("light");
+    document.querySelector(".theme-btn").innerHTML = "☀️";
+  }
+}
